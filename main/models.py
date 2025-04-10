eedbb7b5520d76f8d348b558796eb7d017d45049
WebWrap <149431380+Djole33@users.noreply.github.com>
2025-04-10 13:09:51 +0200

    working with images

--- main/models.py
+++ main/models.py
@@ -13,6 +13,7 @@ class BlogPost(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    blog_image = models.ImageField(upload_to=('blog_images/'))
     description = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
 
