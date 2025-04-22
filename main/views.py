76ffba6eafb8bc996386a88837aea0be5d34607b
WebWrap <149431380+Djole33@users.noreply.github.com>
2025-04-22 13:09:40 +0200

    added homepage for our blogs

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import BlogPost, Category
 
 # Create your views here.
+
+def homepage(request):
+    blog_posts = BlogPost.objects.all()
+    return render(request, 'homepage.html', {'blog_posts': blog_posts})
