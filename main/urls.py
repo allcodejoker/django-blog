0e90a4b0c74f773dad98b783413e17953a3579e6
WebWrap <149431380+Djole33@users.noreply.github.com>
2025-05-03 20:48:52 +0200

    register, login and logout users

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.homepage, name="homepage"),
     path('homepage/', views.homepage, name="homepage"),
     path('blog_post/<int:pk>/', views.blog_post_details, name="blog_post_details"),
+    path('register_user/', views.register_user, name="register_user"),
+    path('login_user/', views.login_user, name="login_user"),
+    path('logout_user/', views.logout_user, name="logout_user"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import BlogPost, Category
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -11,3 +13,50 @@ def blog_post_details(request, pk):
     blog_post = BlogPost.objects.get(id=pk)
 
     return render(request, 'blog_post_details.html', {'blog_post': blog_post})
+
+def register_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        password2 = request.POST["password2"]
+
+        if password != password2:
+            print("Passwords didn't match!")
+            return redirect('register_user')
+
+        if User.objects.filter(username=username).exists():
+            print("Username already taken!")
+            return redirect('register_user')
+
+        try:
+            user = User.objects.create(username=username, password=password)
+            user.save()
+            login(request, user)
+            print("Successfully registered!!!")
+            return redirect('homepage')
+        except Exception as e:
+            print(f"Error: {e}")
+            return redirect('register_user')
+
+    return render(request, 'register_user.html')
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            print("Successfully logged in!!!")
+            return redirect('homepage')
+        else:
+            print("Wrong credentions")
+            return redirect('login_user')
+
+    return render(request, 'login_user.html')
+
+def logout_user(request):
+    logout(request)
+    print("Successfully logged out!!!")
+    return redirect('login_user')
